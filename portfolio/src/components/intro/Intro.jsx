8c58237f72c101a0431d54f2cba0a45466c6926f
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-07-13 17:23:21 -0400

    implemented intro pages with  effects/animation

--- portfolio/src/components/intro/Intro.jsx
+++ portfolio/src/components/intro/Intro.jsx
@@ -1,17 +1,37 @@
-import React from "react";
-import "./Intro.scss"
+import React, { useEffect, useRef } from "react";
+import "./Intro.scss";
+import { init } from "ityped";
+
+let isLoading = true;
 
 export default function Intro() {
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    if (textRef.current && isLoading) {
+      isLoading = false;
+      init(textRef.current, {
+        showCursor: true,
+        backDelay: 1200,
+        typeSpeed: 50,
+        backSpeed: 50,
+        strings: ["Full Stack Web developer", "Persistent Learner", "Problem-solver"],
+      });
+    }
+  }, []);
+
   return (
     <div className="intro" id="intro">
       <div className="wrapper">
         <h2>Hi there, I'm</h2>
         <h1>Hongseoup Yun</h1>
-        <h3><span>Full Stack Web Developer</span></h3>
+        <div className="h3">
+          <b ref={textRef}></b>
+        </div>
       </div>
-      <a>
-        <img src="" alt="arrowDown" />
+      <a className="introArrow" href="#aboutme">
+        <img src="assets/arrow-down.png" />
       </a>
     </div>
-  )
+  );
 }
