09f3d3117121410a5a6c598dc783188d51b447cd
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-07-13 19:48:22 -0400

    adeed basic html for projects

--- portfolio/src/components/projects/Projects.jsx
+++ portfolio/src/components/projects/Projects.jsx
@@ -5,9 +5,16 @@ export default function Projects() {
   return (
     <div className="Projects" id="projects">
       <h1>Projects</h1>
-      <ul>
-        
-      </ul>
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+            </div>
+            <div className="right">
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
