350ccfd68de912efe09d83aaa868e7b450d839d0
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-07-14 22:39:37 -0400

    added basic html for Contact

--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -3,7 +3,17 @@ import "./Contact.scss";
 
 export default function Contact() {
   return (
-  <div className="contact" id="contact">
-    contact</div>
-    )
+    <div className="contact" id="contact">
+      <div className="mainContainer">
+        <div className="tittle">Contact Me</div>
+        <div className="contactLinks">
+          <div className="linkedIn"></div>
+          <div className="gitHub"></div>
+          <div className="instagram"></div>
+          <div className="resume"></div>
+        </div>
+      </div>
+
+    </div>
+  )
 }
