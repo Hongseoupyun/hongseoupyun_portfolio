1d08706b980211aaf28f02ffd57459994f160147
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2023-02-14 14:42:06 -0500

    mobile responsive degin and contents added

--- portfolio/src/components/aboutme/Aboutme.jsx
+++ portfolio/src/components/aboutme/Aboutme.jsx
@@ -1,29 +1,43 @@
 import React from "react";
-import "./Aboutme.scss"
+import "./Aboutme.scss";
 
 export default function Aboutme() {
   return (
     <section className="aboutme" id="aboutme">
       <div className="aboutMeContainer">
         <div className="imgContainer">
-          <img className="aboutMeImg" src="assets/intro-image.png" alt="" />
+          <img className="aboutMeImg" src="assets/profilePic.jpg" alt="" />
         </div>
         <div className="contents">
           <p className="title">ABOUT ME</p>
           <p className="introduction">
-            I am a Full-stack developer who transitioned from the skilled-trade industry as a Technician. <br />In my previous past experience as a Technician, I was always drawn to the analytical mechanics <br />of creating a solution which is very similar to web development. This interest inspired me to pivot <br />my career into a Web Developer.
+            I am a Full-stack developer/Digital Marketing Strategist who
+            transitioned from the skilled-trade industry as a Technician.
+          </p>
+          <p className="introduction">
+            In my previous experience as a Technician, I was always drawn to the
+            analytical mechanics of creating a solution similar to web
+            development. This interest inspired me to pivot my career into a Web
+            Developer. <br />
+            In addition to my technical skills, I also have experience in
+            digital marketing, which has given me a unique perspective on
+            optimizing websites for both functionality and marketing goals.
+            <br />I am always seeking new challenges and opportunities to grow
+            my skills, and I am eager to bring my expertise and drive to a
+            dynamic team. I am responsible, hard-working and constantly learning
+            and growing.
+          </p>
+          <p className="introduction">
+            I am passionate about building well-designed and user-friendly apps
+            with the new skills I learned.
           </p>
 
           <p className="introduction">
-          I am responsible, hard-working and constantly learning and growing. <br />I am passionate in building well-designed and user-friendly apps with new skills i learned.
-          </p>
-
-          <p className="introduction">
-            When I am not at work , I am at home being nice dad and husband, or may be playing tennis on court!
+            When I am not at work , I am at home being nice dad and husband, or
+            may be playing tennis on court!
           </p>
         </div>
-        
       </div>
     </section>
-  )
+  );
 }
