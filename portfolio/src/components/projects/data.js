1f008cc350b1e501c1726ae4f931dfb594ae4954
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2023-08-03 16:32:49 -0400

    added new project

--- portfolio/src/components/projects/data.js
+++ portfolio/src/components/projects/data.js
@@ -1,6 +1,18 @@
 export const projectsDatas = [
   {
     id: 0,
+    title: "Sushi4U",
+    description:
+      "Website for client who owns a sushi restaurant",
+    feature:
+      "Informations about the restaurants including Location, Menus and UberEats. QR Code provided for guests to view menu.",
+    skillStacks: "React, Firebase, Figma",
+    img: "assets/sushi4u.gif",
+    gitHub: "https://github.com/Hongseoupyun/IYD_Sushi4You",
+    demo:"https://sushi4u.ca/"
+  },
+  {
+    id: 1,
     title: "Automated QA for TVO.org",
     description:
       "10 automated test scenarios for TVO.org all docs page using Cypress.",
@@ -11,7 +23,7 @@ export const projectsDatas = [
     gitHub: "https://github.com/Hongseoupyun/TVO_QA_Assignment",
   },
   {
-    id: 1,
+    id: 2,
     title: "SneakersPickers",
     description:
       "SnekersPickers is a trading platform app that users can trade their shoes.",
@@ -24,7 +36,7 @@ export const projectsDatas = [
     demo: "https://sneakerspickers.herokuapp.com/",
   },
   {
-    id: 2,
+    id: 3,
     title: "ShootingGame",
     description: "Web-based Shooting Game developed by Vanila JavaScript/OOP",
     feature: "Hit spacebar to fire bullets to get rid of monsters",
@@ -34,7 +46,7 @@ export const projectsDatas = [
     demo: "https://spaceshipvs.netlify.app/",
   },
   {
-    id: 3,
+    id: 4,
     title: "Scheduler",
     description:
       " A Single Page App scheduler client application to allow users to book interviews with interviewers and schedule data from an API server.",
@@ -46,7 +58,7 @@ export const projectsDatas = [
     demo: undefined,
   },
   {
-    id: 4,
+    id: 5,
     title: "Tweeter",
     description:
       " A Single Page Twitter Clones Application displays a list of tweets entered by user.",
